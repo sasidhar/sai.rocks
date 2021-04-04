422d2f6e402367a042ea16a6c8ccbdc371a4d7e9
Sasidhar Vanga <sasidhar@sasidhar.com>
2021-04-03 18:27:32 -0700

    Taught operators and control structures

---
+++ web-designing-practicals/001-data-types/scripts.js
@@ -0,0 +1,18 @@
+// Data Types
+let name = "Sasidhar";   // String
+let isMarried = true; // true or false
+let age = 41;       // number
+let height = 5.6;   // number
+let undefinedLet;     // undefined
+let nullLet = null;  // null
+
+
+/*
+    * Internal Data Types       typeof (output is string)
+    undefined                   "undefined"
+    null                        "object" (special special case)
+    string                      "string"
+    number (int/float)          "number"
+    boolean                     "boolean"
+*/
+
