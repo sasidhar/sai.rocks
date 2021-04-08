a2a48fc8fa18e381ea6268de3da31d3d26ae52cf
Sai Vanga <ToSaiVanga@gmail.com>
2021-04-08 13:41:07 -0700

    ;

---
+++ web-designing-practicals copy/001-data-types/implicit-coercion.js
@@ -0,0 +1,37 @@
+/*
+
+*/
+
+console.log(1 + 2)
+// 3
+
+console.log("Sasidhar " + "Vanga")
+// Sasidhar Vanga
+
+console.log("a" + 5)
+// a5
+
+console.log("a" + "5")
+// a5
+
+console.log(5 + "5")
+// 55
+
+console.log("5" + 5)
+// 55
+
+console.log(5 + 5)
+// 10
+
+console.log(5 + 5 + "5")
+// 105
+
+console.log("5" + 5 + 5)
+// 555
+
+console.log(5.5 + 1.1 + 2.2)
+// 8.8
+
+console.log(5.5 + 1.1 + "2.2")
+// 6.62.2
+
---
+++ web-designing-practicals copy/007-javascript-functions copy/functions.js
@@ -0,0 +1,24 @@
+/*
+    Minimum Requirements
+    * function name (optional)
+    * inputs/parameters/arguments (optional)
+    * output/return (optional)
+     
+    Structure
+    
+    1. function statement
+
+    function function_name(input1, input2, input3, ...) {
+        // do some math/code/logic ...
+        return output_here;
+    }
+
+    2. function expression
+
+    var function_name = function(input1, input2, input3, ...) {
+        // do some math/code/logic ...
+        return output_here;
+    };
+
+    
+*/
